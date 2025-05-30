7ebeb1e17583aedba98af6808e678e60954a7e57
Adarsh Singh <adarshcomedy4@gmail.com>
2025-05-30 03:55:08 +0000

    changes done in index js file

---
+++ index.js
@@ -0,0 +1,10 @@
+// for(let i=1;i<11;i++){
+//     console.log(`${i} => Hello ,Adarsh ji`);   
+// }
+
+// setTimeout(() => {
+//     console.log(`hi Adarsh sing ji`);
+// }, 1000);
+setInterval(() => {
+    console.log(`hi adarsh singh ji`);
+}, 1000);
